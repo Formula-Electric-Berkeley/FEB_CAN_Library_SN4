ec0d572bb2b370d5af7611d089650c31d748cb54
Aman Doshi <amandoshi001@gmail.com>
2025-02-12 21:09:19 -0800

    Add comments to generate.py

--- generate.py
+++ generate.py
@@ -1,17 +1,22 @@
+# Libraries
 import cantools
 
+# CAN messages
 import bms_messages as bms_msg
 
+# List of functions, used to generate CAN messages.
 MESSAGE_GEN_LIST = [
     bms_msg.get_bms_cell_voltage
 ]
 
 def main():
+    # Load CAN messages
     messages = []
     for i in range(len(MESSAGE_GEN_LIST)):
         msg = MESSAGE_GEN_LIST[i](i)
         messages.append(msg)
     
+    # Generate DBC file
     db = cantools.db.Database(messages=messages)
     cantools.db.dump_file(db, "gen/FEB_CAN.dbc")
 
