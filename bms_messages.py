6c0de28a43ba030c3338d41e0d6c8b5f38d9e8d7
Rahil Pasha <31114117+rahilpasha@users.noreply.github.com>
2025-03-09 13:09:11 -0700

    add ivMeter messages

--- bms_messages.py
+++ bms_messages.py
@@ -38,3 +38,41 @@ def get_bms_cell_voltage(frame_id: int):
     )
 
     return msg
+
+def get_ivMeter_data(frame_id: int):
+    low_range_current = cantools.db.Signal(
+        name="low_range_current",
+        start=0,
+        length=8,
+        byte_order="big_endian",
+        is_signed=False,
+    )
+    
+    high_range_current = cantools.db.Signal(
+        name="high_range_current",
+        start=8,
+        length=8,
+        byte_order="big_endian",
+        is_signed=False,
+    )
+
+    hv_voltage = cantools.db.Signal(
+        name="hv_voltage",
+        start=16,
+        length=8,
+        byte_order="big_endian",
+        is_signed=False,
+        conversion=BaseConversion.factory(scale=1e-3),
+        unit="mV",
+    )
+
+    msg = cantools.db.Message(
+        frame_id=frame_id,
+        name="FEB_IVMeter",
+        length=3,
+        signals=[low_range_current, high_range_current, hv_voltage],
+        comment="ivMeter message for low and high range current and HV voltage.",
+        strict=True
+    )
+
+    return msg
